f7ac9dd0b3efbe044095a7aeae7f4ed0bed21bdf
Omar Rahman <omar@r4men.com>
2025-09-29 05:28:44 -0700

    Enhance external service: add MatchlistV4 and AccountV2 schemas, implement getMatchesByPuuid and getAccountByName methods, and update getMatchById logic

--- src/external/external.service.ts
+++ src/external/external.service.ts
@@ -1,21 +1,69 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { MapsResponse } from './external.schema';
+import { AccountV2, MapsResponse, MatchlistV4 } from './external.schema';
 import { firstValueFrom } from 'rxjs';
+import { AxiosRequestConfig } from 'axios';
+import { CompetitiveSchema } from 'src/competitive/competitive.schema';
 
 @Injectable()
 export class ExternalService implements OnModuleInit {
   private maps: Record<string, string>;
 
+  private axiosConfig: AxiosRequestConfig = {
+    headers: {
+      Authorization: process.env.HENRIK_API_KEY,
+    },
+  };
+
   constructor(private readonly httpService: HttpService) {}
+
+  async getMatchesByPuuid(puuid: string, region: string): Promise<MatchlistV4> {
+    const res = await firstValueFrom(
+      this.httpService.get(
+        `https://api.henrikdev.xyz/valorant/v4/by-puuid/matches/${region}/pc/${puuid}`,
+        this.axiosConfig,
+      ),
+    );
+
+    return MatchlistV4.parse(res.data);
+  }
+
+  async getAccountByName(name: string, tag: string): Promise<AccountV2> {
+    const res = await firstValueFrom(
+      this.httpService.get(
+        `https://api.henrikdev.xyz/valorant/v2/account/${name}/${tag}`,
+        this.axiosConfig,
+      ),
+    );
+
+    return AccountV2.parse(res.data);
+  }
+
+  async getMatchById(id: string, region: string): Promise<CompetitiveSchema> {
+    const config = this.axiosConfig;
+    config.data = {
+      type: 'matchdetails',
+      value: id,
+      region: region,
+    };
+
+    const res = await firstValueFrom(
+      this.httpService.post<{ data: any }>(
+        'https://api.henrikdev.xyz/valorant/v1/raw',
+        this.axiosConfig,
+      ),
+    );
+
+    return CompetitiveSchema.parse(res.data.data);
+  }
 
   @Cron('0 0 * * *')
   async refresh() {
-    await this.fetchMaps();
+    await this.updateMaps();
   }
 
-  async fetchMaps() {
+  async updateMaps() {
     const res = await firstValueFrom(
       this.httpService.get('https://valorant-api.com/v1/maps'),
     );
