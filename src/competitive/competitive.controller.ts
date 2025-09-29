7dd58e7d858561b951cdbcc5fb936df94b0f6de6
Omar Rahman <omar@r4men.com>
2025-09-29 05:28:27 -0700

    Add GET endpoint for testing external service in CompetitiveController

--- src/competitive/competitive.controller.ts
+++ src/competitive/competitive.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CompetitiveSchema } from './competitive.schema';
+import { ExternalService } from 'src/external/external.service';
 
 @Controller('competitive')
 export class CompetitiveController {
+  constructor(private readonly externalService: ExternalService) {}
+
   @Post('trim')
   translateJson(@Body() body: any): CompetitiveSchema {
     return CompetitiveSchema.parse(body);
+  }
+
+  @Get('test')
+  test() {
+    return this.externalService.getMatchesByPuuid(
+      '73130adc-adec-5d41-8002-e8fb99cbd8fc',
+      'na',
+    );
   }
 }
