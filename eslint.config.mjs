5b21e219fef0cc7db9b509d3c4589db4c8b7a6aa
Omar Rahman <omar@r4men.com>
2025-09-29 05:28:10 -0700

    Update ESLint and TypeScript configurations: enable no-unused-expressions and no-unused-vars rules in ESLint, and set noUnusedLocals to true in tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,8 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true }],
       'linebreak-style': ['error', 'unix'],
     },
   },
