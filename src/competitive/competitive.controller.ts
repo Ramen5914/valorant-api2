1292bda4f12d64b4025c33fa4ed7997437766cfb
Omar Rahman <omar@r4men.com>
2025-10-14 12:00:26 -0700

    Add competitive service to controller and implement match saving and retrieval

--- src/competitive/competitive.controller.ts
+++ src/competitive/competitive.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CompetitiveSchema } from './competitive.schema';
 import { ExternalService } from 'src/external/external.service';
+import { Match } from './entities/match.entity';
+import { CompetitiveService } from './competitive.service';
 
 @Controller('competitive')
 export class CompetitiveController {
-  constructor(private readonly externalService: ExternalService) {}
+  constructor(
+    private readonly externalService: ExternalService,
+    private readonly competitiveService: CompetitiveService,
+  ) {}
 
   @Post('trim')
   translateJson(@Body() body: any): CompetitiveSchema {
@@ -17,5 +22,17 @@ export class CompetitiveController {
       '73130adc-adec-5d41-8002-e8fb99cbd8fc',
       'na',
     );
+  }
+
+  @Post('input')
+  async saveMatch(@Body() body: any): Promise<Match> {
+    return await this.competitiveService.createMatch(
+      CompetitiveSchema.parse(body),
+    );
+  }
+
+  @Get(':id')
+  async getMatchById(@Param('id') id: string): Promise<Match | null> {
+    return await this.competitiveService.getMatchById(id);
   }
 }
